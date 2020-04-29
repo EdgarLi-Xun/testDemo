d7a35277d6063562a67495d518db3cebc940ab68
lixun <lixun21521>
2020-04-29 16:27:48 +0800

    添加代码自动生成摸板

---
+++ demo-generator/src/test/java/com/edgarli/demogenerator/DemoGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgarli.demogenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
